b804c4e2b01649bcec69c2e1c0cc89cdb68342d4
themark712 <themark712@yahoo.com>
2025-09-23 12:15:01 -0500

    Add project files.

---
+++ LibraryManagementSystem/Forms/LoginForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LibraryManagementSystem.Forms
+{
+	partial class LoginForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			panel1 = new Panel();
+			label1 = new Label();
+			label2 = new Label();
+			panel1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// panel1
+			// 
+			panel1.BackColor = Color.FromArgb(17, 24, 34);
+			panel1.Controls.Add(label2);
+			panel1.Controls.Add(label1);
+			panel1.Location = new Point(12, 12);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(441, 312);
+			panel1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			label1.BackColor = Color.Yellow;
+			label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label1.Location = new Point(0, -3);
+			label1.Name = "label1";
+			label1.Size = new Size(441, 77);
+			label1.TabIndex = 0;
+			label1.Text = "User Login";
+			label1.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			label2.BackColor = Color.FromArgb(255, 183, 19);
+			label2.Location = new Point(392, 11);
+			label2.Name = "label2";
+			label2.Size = new Size(49, 23);
+			label2.TabIndex = 1;
+			label2.Text = "X";
+			// 
+			// LoginForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(465, 336);
+			Controls.Add(panel1);
+			FormBorderStyle = FormBorderStyle.None;
+			Name = "LoginForm";
+			StartPosition = FormStartPosition.CenterScreen;
+			Text = "Login";
+			panel1.ResumeLayout(false);
+			ResumeLayout(false);
+		}
+
+		#endregion
+
+		private Panel panel1;
+		private Label label1;
+		private Label label2;
+	}
+}
